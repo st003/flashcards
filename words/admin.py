0bda3bcfb1d11e42e31b36a84e64e3eb646af6b6
Stuart Calcote <stuart.j.calcote@gmail.com>
2020-12-20 16:11:03 -0700

    Created ModelAdmin for Word model

--- words/admin.py
+++ words/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Word
 
+
+class WordAdmin(admin.ModelAdmin):
+    """Custom ModelAdmin for Word model."""
+    list_display = ('en', 'jp', 'category')
+
+
 # Register your models here.
-admin.site.register(Word)
+admin.site.register(Word, WordAdmin)
