09c9797b5a0a31d6f499e81e9dcc8af2067b551c
Stuart Calcote <stuart.j.calcote@gmail.com>
2020-12-20 15:59:36 -0700

    Added Word model

--- words/models.py
+++ words/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    """Model defining a word and it's translation."""
+
+    NOUN = 1
+    VERB = 2
+    ADJ = 3
+    CATEGORIES = [
+        (NOUN, 'Noun'),
+        (VERB, 'Verb'),
+        (ADJ, 'Adjective')
+    ]
+
+    en = models.CharField(max_length=64)
+    jp = models.CharField(max_length=64)
+    category = models.IntegerField(null=True, blank=True, choices=CATEGORIES)
+
+    def __str__(self):
+        return self.en
+    
+    def __repr__(self):
+        return f'Word(en={self.en})'
