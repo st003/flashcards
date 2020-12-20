960d39d54e46b2684979f0da40435ea8e273d6f4
Stuart Calcote <stuart.j.calcote@gmail.com>
2020-12-20 16:27:11 -0700

    Added flashcard view

--- words/views.py
+++ words/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'words/index.html')
+
+def flashcard(request):
+    return render(request, 'words/flashcard.html')
