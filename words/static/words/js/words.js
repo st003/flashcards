f5c1a1c6d8d083943a80d897722645f40b223b5a
Stuart Calcote <stuart.j.calcote@gmail.com>
2020-12-26 16:35:05 -0700

    Enligh words now appear first

--- words/static/words/js/words.js
+++ words/static/words/js/words.js
@@ -21,20 +21,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
         document.getElementById('flipCardBtn').addEventListener('click', () => {
             // hide flip button and japanese
-            document.getElementById('jp').style.display = 'none';
+            document.getElementById('en').style.display = 'none';
             document.getElementById('flipCardBtn').style.display = 'none';
             // reveal english text and next button
-            document.getElementById('en').style.display = 'block';
+            document.getElementById('jp').style.display = 'block';
             document.getElementById('newCardBtn').style.display = 'block';
         })
 
         document.getElementById('newCardBtn').addEventListener('click', () => {
             // hide english text and next button
             document.getElementById('newCardBtn').style.display = 'none';
-            document.getElementById('en').style.display = 'none';
+            document.getElementById('jp').style.display = 'none';
             // reveal flip card button and request a new word
             document.getElementById('flipCardBtn').style.display = 'block';
-            document.getElementById('jp').style.display = 'block';
+            document.getElementById('en').style.display = 'block';
             getNewWord(document.getElementById('id').value);
         })
     }
