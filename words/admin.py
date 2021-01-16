4d6c444a127ee40beff0fa42a80f9cadc1076590
Stuart Calcote <stuart.j.calcote@gmail.com>
2021-01-16 15:23:50 -0700

    Added admin UI pagination and filters

--- words/admin.py
+++ words/admin.py
@@ -6,6 +6,8 @@ from .models import Word
 class WordAdmin(admin.ModelAdmin):
     """Custom ModelAdmin for Word model."""
     list_display = ('en', 'jp', 'category')
+    list_per_page = 25
+    list_filter = ('category',)
 
 
 # Register your models here.
