d76a1d11ea30cdf91b89f0930fcac011b68a5c4e
Stuart Calcote <stuart.j.calcote@gmail.com>
2024-06-12 19:49:21 -0700

    Fixed issue where fetch response was not waited for

--- flashcards-app/words/static/words/js/words.js
+++ flashcards-app/words/static/words/js/words.js
@@ -1,8 +1,7 @@
 const { createApp } = Vue
 
 createApp({
-    // required change b/c django uses {{ }}
-    // for its template rendering
+    // required change b/c django uses {{ }} for its template rendering
     delimiters: ['[[', ']]'],
     data() {
         return {
@@ -28,74 +27,88 @@ createApp({
             if (this.category != 0) {
                 this.categories.forEach(c => {
                     if (c.value == this.category) {
-                        categoryName = c.name
+                        categoryName = c.name;
                     }
                 })
             }
-            return categoryName
+            return categoryName;
         }
     },
     created: function () {
         fetch('/api/v1/category/')
             .then(res => res.json())
-            .then(data => { this.categories = data })
+            .then(data => { this.categories = data });
 
         fetch('/api/v1/topic/')
             .then(res => res.json())
-            .then(data => { this.topics = data })
+            .then(data => { this.topics = data });
 
-        this.getNewWord()
+        this.getCard()
+            .then(data => this.setCard(data))
+            .catch(() => this.setError());
     },
     methods: {
-        getNewWord() {
-            const args = new URLSearchParams()
-            if (this.category != 0) args.append('category', this.category)
-            if (this.topic != 'All') args.append('topic', this.topic)
-            if (this.filterChange) args.append('purgeMem', 'true')
+        setCard(data) {
+            this.error = false;
+            this.count = data.count;
+            this.en = data.en;
+            this.jp = data.jp;
+            this.filterChange = false;
+        },
+        setError() {
+            this.count = 0;
+            this.error = true;
+        },
+        getCard() {
+            const args = new URLSearchParams();
+            if (this.category != 0) args.append('category', this.category);
+            if (this.topic != 'All') args.append('topic', this.topic);
+            if (this.filterChange) args.append('purgeMem', 'true');
 
-            fetch('/api/v1/word/rand/?' + args)
-                .then(res => {
-                    if (res.status === 404) return Promise.reject(new Error('404'))
-                    else return Promise.resolve(res.json())
-                })
-                .then(data => {
-                    this.error = false
-                    this.count = data.count
-                    this.en = data.en
-                    this.jp = data.jp
-                    this.filterChange = false
-                })
-                .catch(() => {
-                    this.count = 0
-                    this.error = true
-                })
+            return new Promise((resolve, reject) => {
+                fetch('/api/v1/word/rand/?' + args)
+                    .then(res => {
+                        if (res.status === 404) return Promise.reject(new Error('404'));
+                        else return Promise.resolve(res.json());
+                    })
+                    .then(data => resolve(data))
+                    .catch(() => reject());
+            });
         },
         openFilters() {
-            this.fadeOut = false
-            this.showFilters = true
+            this.fadeOut = false;
+            this.showFilters = true;
         },
         closeFilters() {
             this.fadeOut = true;
-            setTimeout(() => { this.showFilters = false; }, 350)
+            setTimeout(() => { this.showFilters = false; }, 250);
         },
         resetFilters() {
-            this.categorySelection = 0
-            this.topicSelection = 'All'
+            this.categorySelection = 0;
+            this.topicSelection = 'All';
         },
         applyFilters() {
-            this.category = this.categorySelection
-            this.topic = this.topicSelection
-            this.filterChange = true
-            this.cardIsFlipped = false
-            this.getNewWord()
-            this.closeFilters()
+
+            this.category = this.categorySelection;
+            this.topic = this.topicSelection;
+            this.filterChange = true;
+
+            this.getCard()
+                .then(data => this.setCard(data))
+                .catch(() => this.setError())
+                .finally(() => {
+                    this.closeFilters();
+                    this.cardIsFlipped = false;
+                });
         },
         flipCard() {
             this.cardIsFlipped = true;
         },
         nextCard() {
-            this.getNewWord()
-            this.cardIsFlipped = false;
+            this.getCard()
+                .then(data => this.setCard(data))
+                .catch(() => this.setError())
+                .finally(() => { this.cardIsFlipped = false });
         }
     }
-}).mount('#app')
+}).mount('#app');
